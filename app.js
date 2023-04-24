77b834fdab12bdbee4bbac62ca7ec85f4db00770
Alex <asgaroth@yandex.ru>
2023-04-24 20:29:19 +0300

    fix: add alt attr to img tags in HTML according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.className="button delete";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.setAttribute("alt", "");
   deleteButton.appendChild(deleteButtonImg);
 
 
