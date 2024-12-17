e72ee6dee19fbf7e40801aded308d6477e9a39c5
alex <vurdalackoff@gmail.com>
2024-12-17 04:21:31 +0300

    fix: add alt attr to img tags in HTML according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task task-label';
+    label.className="task task-label";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
@@ -45,7 +45,8 @@ var createNewTaskElement=function(taskString){
     editButton.className="edit";
 
     deleteButton.className="delete";
-    deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.src="./remove.svg";
+    deleteButtonImg.setAttribute("alt", "delete");
     deleteButtonImg.className="delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
@@ -84,7 +85,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector("input[type=text]");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("editmode");
